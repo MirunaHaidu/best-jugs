e49ec37556979f99ad90fe74b90d986fcc94d25f
Miruna <miruna_haidu@yahoo.com>
2024-01-03 13:55:07 +0200

    first commit

---
+++ src/main/java/com/demo/bestjugs/BestJugsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.bestjugs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestJugsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestJugsApplication.class, args);
+	}
+
+}
